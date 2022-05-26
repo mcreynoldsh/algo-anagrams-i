daec3b12b6c4ac4537f398b2f1dbb07be91fd594
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-26 13:12:38 -0500

    initial py solution

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,14 @@
 # Don't forget to run your tests!
 
+from operator import truediv
+
+
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	srtdStr1="".join(sorted(string1.lower()))
+	srtdStr2="".join(sorted(string2.lower()))
+	
+	if srtdStr1.strip()==srtdStr2.strip():
+		return True
+	else:
+		return False	
+
